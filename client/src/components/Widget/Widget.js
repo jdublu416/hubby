91f8837e3236915e852408560371643add6da69c
gilkercj <charlesjgilker@gmail.com>
2018-06-25 19:49:50 -0400

    putin widgets components

--- client/src/components/Widget/Widget.js
+++ client/src/components/Widget/Widget.js
@@ -1,26 +1,44 @@
 import "./Widget.css";
 import React, { Component } from 'react'
-
-const  movable = document.querySelector('#movable-box');
+import Rnd from "react-rnd";
+const movable = document.querySelector('#movable-box');
 
 export default class Widget extends Component {
     state = {
+        width: 100,
+        height: 100,
+        x: 100,
+        y: 100,
 
     }
 
 
 
-render() {
-    return (
-        <React.Fragment>
-            <a href="https://www.google.com/">
-                <div className="widget">
+    render() {
+        return (
+            <React.Fragment>
+                {/* <a href="https://www.google.com/"> */}
+                    <Rnd
+                        size={{ width: this.state.width, height: this.state.height }}
+                        position={{ x: this.state.x, y: this.state.y }}
+                        onDragStop={(e, d) => { this.setState({ x: d.x, y: d.y }) }}
+                        onResize={(e, direction, ref, delta, position) => {
+                            this.setState({
+                                width: ref.style.width,
+                                height: ref.style.height,
+                                ...position,
+                            });
+                        }}
+                    >
+                        <div className="widget">
+                            <img src="http://www.funnyordie.com/thumbnails/576e2b3db6/fullsize" />
+                            {/* <h1>Test Porps</h1>
+                            <p>this widget takes you to google</p> */}
+                        </div>
+                    </Rnd>
+                {/* </a> */}
 
-                    <h1>Test Porps</h1>
-                    <p>this widget takes you to google</p>
-                </div>
-            </a>
-        </React.Fragment>
-    )
-}
+            </React.Fragment>
+        )
+    }
 }
