129c384a80331d243b9ee52e33827167e1c4020f
Jdublu <jewljbl13@aol.com>
2018-06-27 12:47:21 -0400

    added backend routes structure files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./pages/Home";
+import Widget from "./components/Widget";
 import './App.css';
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Home" component={Home} />
+        <Route exact path="/" component={Widget} />
+        <Route exact path="/widget" component={Widget} />
       </Switch>
     </div>
   </Router>
