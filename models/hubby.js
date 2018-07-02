1686457faa58e0707fadfeb174ef744ff59e5299
Unknown <timothy.mace@outlook.com>
2018-07-02 18:50:57 -0400

    Mongoose device/widget modeling

--- models/hubby.js
+++ models/hubby.js
@@ -5,10 +5,27 @@ const userSchema = new Schema({
   name: { type: String, required: false },
   userName: { type: String, required: false },
   password: { type: String, required: true },
-  email: {type: String, required:true},
-  date: { type: Date, default: Date.now }
+  email: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  devices: {
+    name: { type: String, required: true },
+    settings: { type: Object, required: false }
+  }
 });
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
+/* For testing porpoises:
+
+  Device settings model
+    settings: {
+      screenRes: {type: Number, required: true},
+      widgets: {
+        WIDGET FILTH HERE
+      }
+    }
+  
+  
+  */
