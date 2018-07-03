79485fa5aad67ba7067cd6f191b1dbc390321512
Jdublu <jewljbl13@aol.com>
2018-07-03 10:59:24 -0400

    code changes for adding a carousel...button components

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,26 +1,38 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import "./Carousel.css";
+import { start } from 'repl';
 
 
-
-<Carousel>
-  <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+<div>
+ <Carousel.Item>
+    <img width={900} height={500} alt="900x500" src="./Images/smartmirrorproject.jpg" />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h3>Bathroom</h3>
+      <p>New possibilities for better time management. Start your day in the know...</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={900} height={500} alt="900x500" src="./Images/imagesfoyer.jpg" />
+    <Carousel.Caption>
+      <h3>Foyer</h3>
+      <p>Glance at your appointments or check the weather on your way out the door!</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img width={900} height={500} alt="900x500" src="/carousel.png" />
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>Side Table</h3>
+      <p>Have access to your contacts and calendar anywhere</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img width={900} height={500} alt="900x500" src="/carousel.png" />
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>DIY hubby</h3>
+      <p>Use our site to power any monitor and utilize the endless possibilities. Simple to make your own!</p>
     </Carousel.Caption>
   </Carousel.Item>
-</Carousel>;
+  </div>
+
+  
