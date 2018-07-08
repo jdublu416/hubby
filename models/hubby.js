645120fec4d15c309088159c81d30e766c66a437
Jdublu <jewljbl13@aol.com>
2018-07-08 16:29:17 -0400

    clean up filth?

--- models/hubby.js
+++ models/hubby.js
@@ -4,9 +4,33 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, required: false },
   userName: { type: String, required: false },
-  password: { type: String, required: true },
-  email: {type: String, required:true},
-  date: { type: Date, default: Date.now }
+  password: { type: String, required: false },
+  email: { type: String, required: false },
+  date: { type: Date, default: Date.now },
+  devices: {
+    name: String,
+    widgets: {
+      weatherAPI: {
+        zipcode: Number,
+        settings: {
+          x: Number,
+          y: Number,
+          height: Number,
+          Width: Number,
+        },
+      },
+      calendar: {
+        appointments: [String],
+        settings: {
+          x: Number,
+          y: Number,
+          height: Number,
+          Width: Number,
+        },
+      }
+    },
+  },
+  
 });
 
 const User = mongoose.model("User", userSchema);
