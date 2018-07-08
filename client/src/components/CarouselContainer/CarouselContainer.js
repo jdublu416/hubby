7b625483b9d9fd6f75a9307af75f25292b71a05e
Jdublu <jewljbl13@aol.com>
2018-07-08 13:14:07 -0400

    working carousel

--- client/src/components/CarouselContainer/CarouselContainer.js
+++ client/src/components/CarouselContainer/CarouselContainer.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import  {Carousel} from 'react-bootstrap';
 import "./CarouselContainer.css";
+import pic from './smartmirrorproject.jpg';
+import foyer from './hall.jpg';
 
 
-export const CarouselContainer = () => {
- 
-<Carousel>
+
+export default class CarouselContainer extends Component {
+
+
+ render(){
+return(
+<Carousel className="carouselContainer">
  <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="./Images/smartmirrorproject.jpg" />
+    <img width={"100%"} height={"auto"} alt="900x500" src={pic} />
     <Carousel.Caption>
       <h3>Bathroom</h3>
       <p>New possibilities for better time management. Start your day in the know...</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="./Images/imagesfoyer.jpg" />
+    <img width={1677} height={550} alt="900x500" src={foyer} />
     <Carousel.Caption>
       <h3>Foyer</h3>
       <p>Glance at your appointments or check the weather on your way out the door!</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+    <img width={900} height={500} alt="900x500" src="./imagesphone.jpg" />
     <Carousel.Caption>
       <h3>Side Table</h3>
       <p>Have access to your contacts and calendar anywhere</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
-    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+    <img width={900} height={500} alt="900x500" src="./images.jpg" />
     <Carousel.Caption>
       <h3>DIY hubby</h3>
       <p>Use our site to power any monitor and utilize the endless possibilities. Simple to make your own!</p>
@@ -37,7 +43,7 @@ export const CarouselContainer = () => {
   </Carousel>
   
 
-  
+)};
 }
 
 
--- client/src/pages/Splash/Splash.js
+++ client/src/pages/Splash/Splash.js
@@ -5,21 +5,26 @@ import CarouselContainer from "../../components/CarouselContainer";
 
 
 
+
+
 const Splash = () => (
   <div>
-  <div>
-  <header>
+      <div>
+        <header>
+          <div>
+            <h1 id="splashh1">hubby.</h1>
+          </div>
+        </header>
+      </div>
+  
+  <CarouselContainer>
     <div>
-      <h1 id="splashh1">hubby.</h1>
-    </div>
-  </header>
-  </div>
-  <CarouselContainer/>
-
-  <div className= "section">
       <a href="/login"><button id="mybtn" className="btn btn-default">Get Started!</button></a>
-  </div>
-  <footer></footer>
+    </div>
+    </CarouselContainer>
+ 
+  <footer>
+  </footer>
   </div>
 );
 
