ac994d5b0f1767bc4531694d107e899f2bb22f3d
Jdublu <jewljbl13@aol.com>
2018-07-15 20:00:59 -0400

    adding bg image to main page
    added dropdown from react bstrap

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -3,6 +3,7 @@ import React from "react";
 // Destructuring the type, className, children and onClick props, applying them to the button element
 const Button = ({ type = "default", className, children, onClick }) => (
   <button
+  
     onClick={onClick}
     className={["btn btn-lg", `btn-${type}`, className].join(" ")}
   >
