7508e744dd58065f568eea3347f590f4eb90bc68
Unknown <timothy.mace@outlook.com>
2018-07-17 19:19:54 -0400

    widget load on login fixed and register fix

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -15,7 +15,7 @@ class Main extends Component {
   // Weather = WeatherAPI;
   constructor(props) {
     super(props);
-    console.log(props.match.params.id);
+    console.log(props.match.params);
   }
   state = {
     activeId: "",
@@ -34,33 +34,63 @@ class Main extends Component {
     calendarWidth: 250,
     calendarX: 250,
     calendarY: 250,
-    data: {},
-  }; 
-   componentDidMount = () => {
-
-     console.log( "on init :" + this.state);
-    setTimeout(this.setState({
-      activeId: this.props.match.params.id
-    }), 500);
+    data: {}
+  };
+  componentDidMount = () => {
+    console.log("on init :" + this.state);
+    setTimeout(
+      this.setState({
+        activeId: this.props.match.params.id
+      }),
+      500
+    );
     this.loadUserSettings();
     // this.changeSettings(this.props.match.params.id);
   };
 
+  handleWidgetLoad = () => {
+    if (this.state.activeWidgetsString.indexOf("TwitterWidget") >= 0) {
+      let newActiveWidgets = [...this.state.activeWidgets];
+      newActiveWidgets.push(TwitterWidget);
+      this.setState({
+        activeWidgets: newActiveWidgets
+      });
+    }
+    if (this.state.activeWidgetsString.indexOf("WeatherAPI") >= 0) {
+      let newActiveWidgets = [...this.state.activeWidgets];
+      newActiveWidgets.push(WeatherAPI);
+      this.setState({
+        activeWidgets: newActiveWidgets
+      });
+    }
+    if (this.state.activeWidgetsString.indexOf("Calendar") >= 0) {
+      let newActiveWidgets = [...this.state.activeWidgets];
+      newActiveWidgets.push(Calendar);
+      this.setState({
+        activeWidgets: newActiveWidgets
+      });
+    }
+    console.log("this is where the magic happens");
+    console.log(this.state);
+  };
 
-  componentDidUpdate(){
-    if(this.state.activeId !== this.props.match.params.id){
+  componentDidUpdate() {
+    if (this.state.activeId !== this.props.match.params.id) {
       this.setState({
         activeId: this.props.match.params.id
-      })
+      });
     }
 
     this.changeSettings(this.props.match.params.id);
-  };
+  }
 
   handleWidgetAdd = event => {
     const val = event.target.getAttribute("value");
 
-    if (val === "Twitter" && this.state.activeWidgets.indexOf(TwitterWidget) < 0) {
+    if (
+      val === "Twitter" &&
+      this.state.activeWidgets.indexOf(TwitterWidget) < 0
+    ) {
       let newActiveWidgets = [...this.state.activeWidgets];
       let newWidgetsString = [...this.state.activeWidgetsString];
       newActiveWidgets.push(TwitterWidget);
@@ -68,7 +98,7 @@ class Main extends Component {
       console.log(newActiveWidgets);
       this.setState({
         activeWidgets: newActiveWidgets,
-        activeWidgetsString: newWidgetsString,
+        activeWidgetsString: newWidgetsString
       });
     } else if (
       val === "WeatherAPI" &&
@@ -81,7 +111,7 @@ class Main extends Component {
       console.log(newActiveWidgets);
       this.setState({
         activeWidgets: newActiveWidgets,
-        activeWidgetsString: newWidgetsString,
+        activeWidgetsString: newWidgetsString
       });
     } else if (
       val === "Calendar" &&
@@ -94,7 +124,7 @@ class Main extends Component {
       console.log(newActiveWidgets);
       this.setState({
         activeWidgets: newActiveWidgets,
-        activeWidgetsString: newWidgetsString,
+        activeWidgetsString: newWidgetsString
       });
     }
   };
@@ -114,12 +144,12 @@ class Main extends Component {
   //      this.props.history.push('/Main/:id/' + res.data._id);
   //   }
   loadUserSettings = event => {
-
+    console.log("loading user settings...");
     API.getUserDataById(this.state.activeId)
       .then(res => {
-        console.log(res.data[0])
+        console.log(res.data[0]);
         let newState = {
-          // activeWidgets: res.data[0].activeWidgets,
+          activeWidgetsString: res.data[0].activeWidgetsString,
           weatherAPIHeight: res.data[0].weatherAPIHeight,
           weatherAPIWidth: res.data[0].weatherAPIWidth,
           weatherAPIX: res.data[0].weatherAPIX,
@@ -131,41 +161,38 @@ class Main extends Component {
           calendarHeight: res.data[0].calendarHeight,
           calendarWidth: res.data[0].calendarWidth,
           calendarX: res.data[0].calendarX,
-          calendarY: res.data[0].calendarY,
-        }
-        this.setState({ ...newState })
-        console.log(this.state)
-      }).catch(err => console.log(err)
-      );
+          calendarY: res.data[0].calendarY
+        };
+        console.log("setting new state...");
+        this.setState({ ...newState });
+        this.handleWidgetLoad();
+        console.log(this.state);
+      })
+      .catch(err => console.log(err));
     // this.setState({ userData: res.data });
   };
 
-
   arrayPersistChecker = () => {
-    API.getUserDataById(this.props.match.params.id)
-      .then(res => {
-        console.log(res.data[0].activeWidgetsString[0])
-  
-  })
-}
-
+    API.getUserDataById(this.props.match.params.id).then(res => {
+      console.log(res.data[0].activeWidgetsString[0]);
+    });
+  };
 
   loadWidgetSettings = activeId => {
     console.log("widget load instaciated");
-    API.getUserDataById
-  }
+    API.getUserDataById;
+  };
 
   newData;
 
   widgetOnChangeHandler = event => {
-    console.log("widget change handled: " + event)
+    console.log("widget change handled: " + event);
     const newData = event;
-    console.log("hope this is data: " + newData.weatherAPIHeight)
-    this.setState({...newData})
-  }
+    console.log("hope this is data: " + newData.weatherAPIHeight);
+    this.setState({ ...newData });
+  };
 
   changeSettings = activeId => {
-
     // console.log("cha cha cha changing and id: " + activeId);
 
     API.updateUserData(activeId, {
@@ -182,10 +209,10 @@ class Main extends Component {
         calendarWidth: this.state.calendarWidth,
         calendarHeight: this.state.calendarHeight,
         calendarX: this.state.calendarX,
-        calendarY: this.state.calendarY,
+        calendarY: this.state.calendarY
       }
     })
-    // .then(console.log("through changesettings call"))
+      // .then(console.log("through changesettings call"))
       // .then(activeId => this.props.history.push("/Main/:id"))
       .catch(err => console.log(err));
   };
@@ -200,25 +227,51 @@ class Main extends Component {
           <Widget
             key={i}
             type={this.state.activeWidgets[i]}
-            thisWidget={i === this.state.activeWidgets.indexOf(WeatherAPI) ? "WeatherAPI" :
-              i === this.state.activeWidgets.indexOf(TwitterWidget) ? "TwitterWidget" :
-              i === this.state.activeWidgets.indexOf(Calendar) ? "Calendar" : null}
+            thisWidget={
+              i === this.state.activeWidgets.indexOf(WeatherAPI)
+                ? "WeatherAPI"
+                : i === this.state.activeWidgets.indexOf(TwitterWidget)
+                  ? "TwitterWidget"
+                  : i === this.state.activeWidgets.indexOf(Calendar)
+                    ? "Calendar"
+                    : null
+            }
             height={
-                i === this.state.activeWidgets.indexOf(WeatherAPI) ? this.state.weatherAPIHeight :
-                i === this.state.activeWidgets.indexOf(TwitterWidget) ? this.state.twitterHeight :
-                i === this.state.activeWidgets.indexOf(Calendar) ? this.state.calendarHeight : null}
+              i === this.state.activeWidgets.indexOf(WeatherAPI)
+                ? this.state.weatherAPIHeight
+                : i === this.state.activeWidgets.indexOf(TwitterWidget)
+                  ? this.state.twitterHeight
+                  : i === this.state.activeWidgets.indexOf(Calendar)
+                    ? this.state.calendarHeight
+                    : null
+            }
             width={
-                i === this.state.activeWidgets.indexOf(WeatherAPI) ? this.state.weatherAPIWidth :
-                i === this.state.activeWidgets.indexOf(TwitterWidget) ? this.state.twitterWidth :
-                i === this.state.activeWidgets.indexOf(Calendar) ? this.state.calendarWidth : null}
+              i === this.state.activeWidgets.indexOf(WeatherAPI)
+                ? this.state.weatherAPIWidth
+                : i === this.state.activeWidgets.indexOf(TwitterWidget)
+                  ? this.state.twitterWidth
+                  : i === this.state.activeWidgets.indexOf(Calendar)
+                    ? this.state.calendarWidth
+                    : null
+            }
             x={
-                i === this.state.activeWidgets.indexOf(WeatherAPI) ? this.state.weatherAPIX :
-                i === this.state.activeWidgets.indexOf(TwitterWidget) ? this.state.twitterX :
-                i === this.state.activeWidgets.indexOf(Calendar) ? this.state.calendarX : null}
+              i === this.state.activeWidgets.indexOf(WeatherAPI)
+                ? this.state.weatherAPIX
+                : i === this.state.activeWidgets.indexOf(TwitterWidget)
+                  ? this.state.twitterX
+                  : i === this.state.activeWidgets.indexOf(Calendar)
+                    ? this.state.calendarX
+                    : null
+            }
             y={
-                i === this.state.activeWidgets.indexOf(WeatherAPI) ? this.state.weatherAPIY :
-                i === this.state.activeWidgets.indexOf(TwitterWidget) ? this.state.twitterY :
-                i === this.state.activeWidgets.indexOf(Calendar) ? this.state.calendarY : null}
+              i === this.state.activeWidgets.indexOf(WeatherAPI)
+                ? this.state.weatherAPIY
+                : i === this.state.activeWidgets.indexOf(TwitterWidget)
+                  ? this.state.twitterY
+                  : i === this.state.activeWidgets.indexOf(Calendar)
+                    ? this.state.calendarY
+                    : null
+            }
             draggable={this.state.staticMode}
             resizable={this.state.staticMode == true ? false : true}
             changeHandler={this.widgetOnChangeHandler}
@@ -254,11 +307,7 @@ class Main extends Component {
             Calendar
           </MenuItem>
 
-          <MenuItem
-            eventKey="3"
-            value="Twitter"
-            onClick={this.handleWidgetAdd}
-          >
+          <MenuItem eventKey="3" value="Twitter" onClick={this.handleWidgetAdd}>
             Twitter
           </MenuItem>
         </DropdownButton>
